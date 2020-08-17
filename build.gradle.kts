f2285b3ffb5ed75cf347dc6c5d34e029d7a6ed2d
Shuichi Sato <shuichi.sato@linecorp.com>
2020-08-17 21:03:04 +0900

    kotlinize

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,33 @@
 plugins {
-    id("org.jetbrains.intellij") version "0.4.10"
-    java
+    id("org.jetbrains.intellij") version "0.4.21"
+    id("org.jetbrains.kotlin.jvm") version "1.3.70"
 }
-
-group = "com.github.syuchan1005"
-version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testCompile("junit", "junit", "4.12")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.70")
 }
 
-// See https://github.com/JetBrains/gradle-intellij-plugin/
-intellij {
-    version = "IC-2019.2.3"
-
+apply {
+    plugin("idea")
+    plugin("org.jetbrains.intellij")
+    plugin("kotlin")
+    plugin("java")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+group = "com.github.syuchan1005"
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    // version = "IC-2019.2.3"
 }
 
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
--- src/main/java/com/github/syuchan1005/revealinprojectview/RevealFileAction.kt
+++ src/main/java/com/github/syuchan1005/revealinprojectview/RevealFileAction.kt
@@ -1,89 +1,60 @@
-package com.github.syuchan1005.revealinprojectview;
+package com.github.syuchan1005.revealinprojectview
 
-import com.intellij.ide.FileEditorProvider;
-import com.intellij.ide.SelectInContext;
-import com.intellij.ide.SelectInTarget;
-import com.intellij.ide.actions.ShowFilePathAction;
-import com.intellij.ide.projectView.ProjectView;
-import com.intellij.ide.projectView.impl.AbstractProjectViewPane;
-import com.intellij.ide.projectView.impl.ProjectViewPane;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.Presentation;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.ide.FileEditorProvider
+import com.intellij.ide.SelectInContext
+import com.intellij.ide.actions.ShowFilePathAction
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.ide.projectView.impl.ProjectViewPane
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
 
-public class RevealFileAction extends DumbAwareAction {
-    @Override
-    public void update(@NotNull AnActionEvent e) {
-        Project project = e.getProject();
-        if (project == null) return;
-        ProjectView projectView = ProjectView.getInstance(project);
-        VirtualFile file = ShowFilePathAction.findLocalFile(CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext()));
-        Presentation presentation = e.getPresentation();
-        presentation.setEnabled(!projectView.getCurrentViewId().equals(ProjectViewPane.ID) && (file != null && !file.isDirectory()));
+class RevealFileAction : DumbAwareAction() {
+    override fun update(e: AnActionEvent) {
+        val project = e.project ?: return
+        val projectView = ProjectView.getInstance(project)
+        val file = ShowFilePathAction.findLocalFile(CommonDataKeys.VIRTUAL_FILE.getData(e.dataContext))
+        val presentation = e.presentation
+        presentation.isEnabled = projectView.currentViewId != ProjectViewPane.ID && file != null && !file.isDirectory
     }
 
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        Project project = e.getProject();
-        VirtualFile virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-        if (project == null || virtualFile == null) return;
-        PsiFile psiFile = PsiManager.getInstance(project).findFile(virtualFile);
-        if (psiFile == null) return;
-
-        ProjectView projectView = ProjectView.getInstance(project);
-        projectView.changeView(ProjectViewPane.ID);
-        AbstractProjectViewPane currentProjectViewPane = projectView.getCurrentProjectViewPane();
-        SelectInTarget selectInTarget = currentProjectViewPane.createSelectInTarget();
-        selectInTarget.selectIn(new MySelectInContext(psiFile, project), false);
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project
+        val virtualFile = e.getData(CommonDataKeys.VIRTUAL_FILE)
+        if (project == null || virtualFile == null) return
+        val psiFile = PsiManager.getInstance(project).findFile(virtualFile) ?: return
+        val projectView = ProjectView.getInstance(project)
+        projectView.changeView(ProjectViewPane.ID)
+        val currentProjectViewPane = projectView.currentProjectViewPane
+        val selectInTarget = currentProjectViewPane.createSelectInTarget()
+        selectInTarget.selectIn(MySelectInContext(psiFile, project), false)
     }
 
-    public static class MySelectInContext implements SelectInContext {
-        private final PsiFile myPsiFile;
-        private final Project myProject;
-
-        public MySelectInContext(@NotNull PsiFile psiFile, @NotNull Project myProject) {
-            myPsiFile = psiFile;
-            this.myProject = myProject;
+    class MySelectInContext(private val psiFile: PsiFile, private val myProject: Project) : SelectInContext {
+        override fun getProject(): Project {
+            return myProject
         }
 
-        @Override
-        @NotNull
-        public Project getProject() {
-            return myProject;
+        override fun getFileEditorProvider(): FileEditorProvider {
+            return FileEditorProvider { FileEditorManager.getInstance(project).openFile(virtualFile, false)[0] }
         }
 
-        @NotNull
-        private PsiFile getPsiFile() {
-            return myPsiFile;
+        private val psiElement: PsiElement
+            get() = psiFile
+
+        override fun getVirtualFile(): VirtualFile {
+            return psiFile.virtualFile
         }
 
-        @Override
-        @NotNull
-        public FileEditorProvider getFileEditorProvider() {
-            return null;
+        override fun getSelectorInFile(): Any? {
+            return psiElement
         }
 
-        @NotNull
-        private PsiElement getPsiElement() {
-            return myPsiFile;
-        }
-
-        @Override
-        @NotNull
-        public VirtualFile getVirtualFile() {
-            return getPsiFile().getVirtualFile();
-        }
-
-        @Override
-        public Object getSelectorInFile() {
-            return getPsiElement();
-        }
     }
 }
